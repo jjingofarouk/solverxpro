b654c06597a0776de28efaa3cd4183e7da89da66
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-08 12:15:01 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { sendToVercelAnalytics } from './vitals';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,4 +10,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-reportWebVitals(sendToVercelAnalytics);
+serviceWorker.unregister();
